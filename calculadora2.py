bbab17be4e9207571a8da443b30c33a33211457f
Leonardo <leocolaco15@hotmail.com>
2026-09-11 14:14:33 -0300

    Add a new metod for calculator

---
+++ calculadora2.py
@@ -0,0 +1,15 @@
+num1 = int(input('Digite o primeiro numero: '))
+num2 = int(input('Digite o segundo numero: '))
+operacao = input('Digite a operação: ')
+
+match operacao:
+    case '+':
+        res = num1 + num2
+    case '-':
+        res = num1 - num2
+    case '*':
+        res = num1 * num2
+    case '/':
+        res = num1 / num2
+
+print(f'O seu resultado é {res}')
